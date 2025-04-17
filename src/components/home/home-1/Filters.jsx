325dce049144dbc7238a109ae415bd28ff72c0c9
goutham <uppinurigouthamreddy@gmail.com>
2025-04-17 23:50:51 +0530

    final release

--- src/components/home/home-1/Filters.jsx
+++ src/components/home/home-1/Filters.jsx
@@ -15,7 +15,7 @@ const Filters = () => {
 
   const fetchProperties = async () => {
     try {
-      const response = await fetch("https://xbfakjw2ee.execute-api.ap-south-1.amazonaws.com/dev/get-properties");
+      const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/get-properties`);
       const data = await response.json();
       setProperties(data.data || []);
     } catch (error) {
